da9fe071c98a6e32e5671ddca36fa47e2e74c165
Teresa Spencer <teresalandkamer@gmail.com>
2025-11-10 08:16:20 -0600

    Add initial implementation of utility functions and app initialization logic
    - Created utility functions: createElemWithText, createSelectOptions, toggleCommentSection, toggleCommentButton, deleteChildElements.
    - Implemented functions for handling user data and posts: getUsers, getUserPosts, getUser, getPostComments.
    - Developed functions for displaying comments and posts: createComments, createPosts, displayComments, displayPosts.
    - Added event handling functions: addButtonListeners, removeButtonListeners, refreshPosts, selectMenuChangeEventHandler.
    - Established app initialization functions: initPage, initApp, ensuring proper event listeners are set up.
    - Documented function dependencies and usage for clarity and future development.

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// 1.
+function createElemWithText(tag, text, className) {
+    if (//no tag) {
+    // tag = "p";
+    }
+    if (//no text) {
+        text = "";
+    }
+    document.createElement(tag);
+    return tag;
+}
+
+// 2.
+function createSelectOptions() {
+
+}
+
+// 3.
+function toggleCommentSection() {
+
+}
+
+// 4.
+function toggleCommentButton() {
+
+}
+
+// 5.
+function deleteChildElements() {
+
+}
+
+// 6.
+function addButtonListeners() {
+
+}
+
+// 7.
+function removeButtonListeners() {
+
+}
+
+// 8.
+function createComments() {
+
+}
+
+// 9.
+function populateSelectMenu() {
+
+}
+
+// 10.
+function getUsers() {
+
+}
+
+// 11.
+function getUserPosts() {
+
+}
+
+// 12.
+function getUser() {
+
+}
+
+// 13.
+function getPostComments() {
+    
+}
